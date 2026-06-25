ac0585fb1a7c6c409ebabfdfdc3413bd08e74073
mrcydn2000@hotmail.com <mrcydn2000@hotmail.com>
2026-06-25 18:22:09 +0000

    create intermediate orders operational model

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -9,4 +9,4 @@ select operational.orders_id,
     operational.margin + ship.shipping_fee - ship.logcost - ship.ship_cost as operational_margin
  from operational
 left join ship 
-ON operational.orders_id = ship.orders_id 
+ON operational.orders_id = ship.orders_id   
